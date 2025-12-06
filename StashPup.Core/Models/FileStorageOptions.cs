a176e5639acca77c183b60a00d3c24213fc192db
Cody <cydoentis@gmail.com>
2025-12-05 21:10:58 -0500

    refactor: FileStorageOptions.cs NamingStrategy is now optional

--- StashPup.Core/Models/FileStorageOptions.cs
+++ StashPup.Core/Models/FileStorageOptions.cs
@@ -5,5 +5,5 @@ public class FileStorageOptions
     public long? MaxFileSizeBytes { get; set; }
     public List<string> AllowableExtensions { get; set; } = [];
     public bool HashFileName { get; set; }
-    public Func<string, string> NamingStrategy { get; set; }
+    public Func<string, string>? NamingStrategy { get; set; }
 }
