38578be6842a857b9c275476bda572b3aebe14d0
Cody <cydoentis@gmail.com>
2025-12-05 21:21:36 -0500

    feat: added a paginated result class for listing files

---
+++ StashPup.Core/Models/PaginatedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace StashPup.Core.Models;
+
+public class PaginatedResult<T>
+{
+    public IReadOnlyList<T> Items { get; set; } = new List<T>();
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalItems { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalItems / PageSize);
+}
